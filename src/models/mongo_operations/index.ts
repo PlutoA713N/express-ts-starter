576b06a48e77d89591aafaafbb28099807d3efd3
PlutoA713N <mallachandu99@gmail.com>
2025-05-30 01:30:41 +0530

    fix: define the return type

--- src/models/mongo_operations/index.ts
+++ src/models/mongo_operations/index.ts
@@ -4,18 +4,19 @@ import logger from "../../logger";
 import {createSuccessResponse} from "../../factory/create.sucess.response";
 import {createApiError} from "../../factory/create.api.error";
 
-export async function mongoHealthcheck(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+export async function mongoHealthcheck(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
         await pingDB();
         logger.info("MongoDB ping successful");
 
-        return res.status(200).json(
+         res.status(200).json(
             createSuccessResponse({
                 code: "MONGO_OK",
                 message: "MongoDB connected",
                 data: { mongo: "connected" }
             })
         );
+         return
     } catch (err) {
         logger.error("MongoDB health check failed", err);
 
