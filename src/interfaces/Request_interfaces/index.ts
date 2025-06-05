224e2e1d0550d0d38c1f5e62a3f32b5252490f24
PlutoA713N <mallachandu99@gmail.com>
2025-06-05 06:04:49 +0530

    chore: delete post model import

--- src/interfaces/Request_interfaces/index.ts
+++ src/interfaces/Request_interfaces/index.ts
@@ -1,10 +1,8 @@
 import {IUser} from "../../models/user/user.model";
-import {IPost} from "../../models/post/post.model";
 import {Request} from "express";
 
 export interface ICustomRequest extends Request  {
     user?: IUser
-    post?: IPost
 }
 
 export type AttachField = Extract<keyof ICustomRequest, string>;
